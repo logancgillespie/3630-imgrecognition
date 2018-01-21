76de1da774c2bc62cfa452e2a0f7d03bb49ed1ae
logancgillespie <logancgillespie@gmail.com>
2018-01-21 14:05:52 -0500

    done

--- imgclassification.py
+++ imgclassification.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 ##############
 #### Your name: Logan Gillespie
@@ -9,8 +9,9 @@ import re
 from sklearn import svm, metrics
 from skimage import io, feature, filters, exposure, color
 
+
 class ImageClassifier:
-    
+
     def __init__(self):
         self.classifer = None
 
@@ -19,44 +20,47 @@ class ImageClassifier:
 
     def load_data_from_folder(self, dir):
         # read all images into an image collection
-        ic = io.ImageCollection(dir+"*.bmp", load_func=self.imread_convert)
-        
-        #create one large array of image data
+        ic = io.ImageCollection(dir + "*.bmp", load_func=self.imread_convert)
+
+        # create one large array of image data
         data = io.concatenate_images(ic)
-        
-        #extract labels from image names
+
+        # extract labels from image names
         labels = np.array(ic.files)
         for i, f in enumerate(labels):
             m = re.search("_", f)
             labels[i] = f[len(dir):m.start()]
-        return(data,labels)
+        return (data, labels)
 
     def extract_image_features(self, data):
         # Please do not modify the header above
 
         # extract feature vector from image data
-
         ########################
         ######## YOUR CODE HERE
         ########################
         feature_data = []
+        # np.zeros((len(data), 240, 320))
         for i in range(0, len(data)):
             image = color.rgb2gray(data[i])
-            feature_data.append(feature.hog(image))
+
+            hog = feature.hog(image, pixels_per_cell=(48, 48), cells_per_block=(4, 4), orientations=9,
+                              feature_vector=True, block_norm='L2-Hys')
+
+            feature_data.append(hog)
 
         # Please do not modify the return type below
-        return(feature_data)
+        return (feature_data)
 
     def train_classifier(self, train_data, train_labels):
         # Please do not modify the header above
-        
+
         # train model and save the trained model to self.classifier
-        
+
         ########################
         ######## YOUR CODE HERE
         ########################
-        print(train_data)
-        clf = svm.SVC()
+        clf = svm.LinearSVC()
         clf.fit(train_data, train_labels)
         self.classifer = clf
 
@@ -65,44 +69,42 @@ class ImageClassifier:
 
         # predict labels of test data using trained model in self.classifier
         # the code below expects output to be stored in predicted_labels
-        
+
         ########################
         ######## YOUR CODE HERE
         ########################
         # Please do not modify the return type below
         predict = self.classifer.predict(data)
-        print(predict)
         predicted_labels = predict
 
         return predicted_labels
 
-      
-def main():
 
+def main():
     img_clf = ImageClassifier()
 
     # load images
     (train_raw, train_labels) = img_clf.load_data_from_folder('./train/')
     (test_raw, test_labels) = img_clf.load_data_from_folder('./test/')
-    
+
     # convert images into features
     train_data = img_clf.extract_image_features(train_raw)
     test_data = img_clf.extract_image_features(test_raw)
-    
+
     # train model and test on training data
     img_clf.train_classifier(train_data, train_labels)
     predicted_labels = img_clf.predict_labels(train_data)
     print("\nTraining results")
     print("=============================")
-    print("Confusion Matrix:\n",metrics.confusion_matrix(train_labels, predicted_labels))
+    print("Confusion Matrix:\n", metrics.confusion_matrix(train_labels, predicted_labels))
     print("Accuracy: ", metrics.accuracy_score(train_labels, predicted_labels))
     print("F1 score: ", metrics.f1_score(train_labels, predicted_labels, average='micro'))
-    
+
     # test model
     predicted_labels = img_clf.predict_labels(test_data)
-    print("\nTraining results")
+    print("\nTesting results")
     print("=============================")
-    print("Confusion Matrix:\n",metrics.confusion_matrix(test_labels, predicted_labels))
+    print("Confusion Matrix:\n", metrics.confusion_matrix(test_labels, predicted_labels))
     print("Accuracy: ", metrics.accuracy_score(test_labels, predicted_labels))
     print("F1 score: ", metrics.f1_score(test_labels, predicted_labels, average='micro'))
 
